23d8a0813179311b1b6e1ecdd3e2bed08f27a6ae
2022211001000404GaoWenyang <2428098726@qq.com>
2024-04-18 23:02:10 +0800

    HW-Week 7 Commit

---
+++ src/main/java/com/GaoWenyang/controller/HomeServlet.java
@@ -0,0 +1,22 @@
+package com.GaoWenyang.controller;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet("/home")
+public class HomeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+// when click a link -method is get
+        //come here
+        //fowward to view(index.jsp)
+        request.getRequestDispatcher("WEB-INF/views/index.jsp").forward(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
